51bc8cb0fac474aa05e65e9a8168fa318725189d
Ilya Cherkasov <ilyakopeysk@gmail.com>
2022-04-16 16:42:45 +0300

    Animate companies

--- src/components/Companies.tsx
+++ src/components/Companies.tsx
@@ -4,6 +4,7 @@ import { ReactComponent as HqoSvg } from 'assets/hqo.svg';
 import { ReactComponent as ItransitionSvg } from 'assets/itransition.svg';
 import { ReactComponent as KandaSvg } from 'assets/kanda.svg';
 import { ReactComponent as PuncherSvg } from 'assets/puncher.svg';
+import { motion } from 'framer-motion';
 
 const Container = styled.div`
   display: flex;
@@ -19,21 +20,53 @@ const Container = styled.div`
   }
 `;
 
+const companies = [
+  {
+    title: 'PuncherApp',
+    href: 'https://puncher.app',
+    logo: PuncherSvg,
+  },
+  {
+    title: 'Itransition',
+    href: 'https://itransition.com',
+    logo: ItransitionSvg,
+  },
+  {
+    title: 'Kanda Software',
+    href: 'https://kandasoft.com',
+    logo: KandaSvg,
+  },
+  {
+    title: 'HqO',
+    href: 'https://hqo.com',
+    logo: HqoSvg,
+  },
+];
+
 const Companies = () => (
   <Section id="companies" header="Companies" subheader="Where I Worked">
     <Container>
-      <a href="https://puncher.app" target="_blank" rel="noreferrer">
-        <PuncherSvg />
-      </a>
-      <a href="https://itransition.com" target="_blank" rel="noreferrer">
-        <ItransitionSvg />
-      </a>
-      <a href="https://kandasoft.com" target="_blank" rel="noreferrer">
-        <KandaSvg />
-      </a>
-      <a href="https://hqo.com" target="_blank" rel="noreferrer">
-        <HqoSvg />
-      </a>
+      {companies.map((company) => (
+        <motion.a
+          key={company.title}
+          title={company.title}
+          href={company.href}
+          target="_blank"
+          rel="noreferrer"
+          variants={{
+            hidden: { y: 100, opacity: 0 },
+            visible: { y: 0, opacity: 1, transition: { duration: 1 } },
+            hovered: { scale: 1.5 },
+          }}
+          whileHover="hovered"
+          whileTap="hovered"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+        >
+          <company.logo />
+        </motion.a>
+      ))}
     </Container>
   </Section>
 );
