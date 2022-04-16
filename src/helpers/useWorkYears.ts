fcb64aadf948357a97417d1478b9783b7ec6cb1f
Ilya Cherkasov <ilyakopeysk@gmail.com>
2022-04-16 13:02:49 +0300

    Fix real work start month

--- src/helpers/useWorkYears.ts
+++ src/helpers/useWorkYears.ts
@@ -1,6 +1,6 @@
 const useWorkYears = () => {
   const now = new Date();
-  const startWork = new Date(2019, 6, 18);
+  const startWork = new Date(2019, 5, 18);
   const year = 1000 * 60 * 60 * 24 * 365;
 
   const difference = Math.floor(now.getTime() - startWork.getTime());
